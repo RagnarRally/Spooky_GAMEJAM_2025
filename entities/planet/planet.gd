64fe8da8c2ecda46f730f6bf61edf423cbe43215
Edvin <36007073+Errif@users.noreply.github.com>
2025-10-18 19:17:21 +0200

    added function for planet explosion

--- entities/planet/planet.gd
+++ entities/planet/planet.gd
@@ -6,6 +6,8 @@ class_name Planet
 @export var _graphics_root: Node2D
 @export var collision_shape: CollisionShape2D
 
+@export var explosion_particles: GPUParticles2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 
@@ -16,4 +18,11 @@ func setup(new_properties: PlanetProperties):
 
 	_graphics_root.scale = Vector2.ONE * new_properties.size / 10
 	collision_shape.shape.radius = new_properties.size
-	
+
+
+func explode():
+	explosion_particles.emitting = true
+
+	await get_tree().create_timer(1).timeout
+
+	queue_free()
