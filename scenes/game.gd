34f804187a7ca0b2d878f132ecf22f52c0aca5e4
Edvin <36007073+Errif@users.noreply.github.com>
2025-10-18 13:16:36 +0200

    added some sound

--- scenes/game.gd
+++ scenes/game.gd
@@ -6,7 +6,7 @@ var zoomed: bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	AudioManager.change_music("game_music")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
