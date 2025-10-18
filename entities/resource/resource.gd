52d59d72853164e92902e2f770f4ce99b849df03
Ragnar Rally <145118951+RagnarRally@users.noreply.github.com>
2025-10-18 21:05:31 +0200

    latest

--- entities/resource/resource.gd
+++ entities/resource/resource.gd
@@ -4,4 +4,4 @@ func _on_body_entered(body: Node2D) -> void:
 	if (Globals.healthPoints < 3):
 		get_node("/root/Game/CanvasLayer/HBoxContainer").get_child(Globals.healthPoints).texture = Globals.HEART_FULL
 		Globals.healthPoints += 1
-		queue_free()
+	queue_free()
